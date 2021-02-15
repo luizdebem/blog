989f94ebde0e614045191d650af9add4becd175e
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-02-15 19:52:22 -0300

    finally fixed a footer css bug that was messing up a lot of things

--- components/Layout.js
+++ components/Layout.js
@@ -15,17 +15,19 @@ const Layout = ({ children }) => {
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"></link>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous" />
-        
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
       </Head>
 
-      <Header />
+      <div className="wrapper">
+        <Header />
 
-      <main className="layout-main">
-        { children }
-      </main>
+        <main className="layout-main">
+          {children}
+        </main>
 
-      <Footer />
+        <Footer />
+      </div>
     </>
   )
 }
